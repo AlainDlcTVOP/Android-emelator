f45d1827d1e09ff7d534dbc36121ecacea2775ec
AlainDlcTVOP <alain_dlc@outlook.com>
2021-04-08 18:39:46 +0200

    fecth data

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album List</Text>
-        </View>
-    );
-};
+// just for testing the key
+const apikey = 'apikey=MzY4ZGJjYmUtMzNmZi00YjliLWIwNGEtZDg2NTJhM2Q0MzM0';
+const trackList = 'https://api.napster.com/v2.1/tracks/top?';
+
+class AlbumList extends Component {
+
+    state = { albums: [] };
+
+    UNSAFE_componentWillMount() {
+        fetch(trackList + apikey)
+            .then((response) => response.json())
+            .then((responseData) => this.setState({ albums: responseData }));
+
+    }
+    render() {
+        console.log(this.state);
+        return (
+            <View>
+                <Text>Album List</Text>
+            </View>
+        );
+    }
+}
+
 
 export default AlbumList;
