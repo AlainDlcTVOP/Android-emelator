36411144bc0eff39a7bf71ca20915c3ed8c14c21
AlainDlcTVOP <alain_dlc@outlook.com>
2021-04-27 20:55:22 +0200

    dummy data.json

---
+++ albums/src/components/Card.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Card = (props) => {
+    return (
+        <View style={styles.containerStyle}>
+            {props.children}
+        </View>
+    );
+};
+
+const styles = {
+    containerStyle: {
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0, // hide the border
+        ShadowColor: '#000',
+        ShadowOffset: { width: 0, hight: 2 },
+        ShadowOpacity: 0.1,
+        ShadowRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10
+
+    }
+};
+export default Card;
