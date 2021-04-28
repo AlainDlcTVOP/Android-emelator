b2e6bcc73d580a24783419b862fe15c56eba4a52
AlainDlcTVOP <alain_dlc@outlook.com>
2021-04-28 18:11:24 +0200

    url buy button

---
+++ albums/src/data/data.js
@@ -0,0 +1,11 @@
+[
+    {
+        "albums": {
+            "title": "Tylor Swift",
+            "artist": "Tylor Swift",
+            "url": "https://www.amazon.com/Taylor-Swift/dp/B0014I4KH6",
+            "image": "http://www.pngall.com/wp-content/uploads/2016/06/Taylor-Swift-Free-Download-PNG.png",
+            "thumbnail_image": "https://i.imgur.com/K3KJ3w4h.jpg"
+        }
+    }
+]
