6efc6739e0ce519cf424f942298f747edde07454
AlainDlcTVOP <alain_dlc@outlook.com>
2021-04-29 07:52:18 +0200

    aut-project setup

---
+++ auth/src/components/common/Button.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPress, children }) => {
+    const { buttomStyle, textStyle } = styles;
+    return (
+        <TouchableOpacity onPress={onPress} style={buttomStyle}>
+            <Text style={textStyle}>
+                {children}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = {
+    buttomStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
+    },
+    textStyle: {
+        flex: 1,
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+
+    }
+};
+export { Button };
